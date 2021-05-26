95d91c276575fac23ebb154d7571a88bc7d2953f
buckn <nathanieljbuck7@gmail.com>
2021-05-25 20:35:31 -0400

    fixed square wave

--- snd.h
+++ snd.h
@@ -2,8 +2,8 @@ void snd_clbk(float* buffer, int num_frames, int num_channels) {
 	printf("%d", num_channels);
     for (int i = 0; i < num_frames; i++) {
     	if (cli.keys_down[SAPP_KEYCODE_SPACE]) {
-    		buffer[2*i + 0] = (i % 168 == 0);  // left channel
-        	buffer[2*i + 1] = (i % 168 == 0);  // right channel
+    		buffer[2*i + 0] = (i % 168 > 83);  // left channel
+        	buffer[2*i + 1] = (i % 168 < 83);  // right channel
     	} else {
     		buffer[2*i + 0] = 0;  // left channel
         	buffer[2*i + 1] = 0;  // right channel
@@ -14,6 +14,7 @@ void snd_clbk(float* buffer, int num_frames, int num_channels) {
 void snd_init() {
 	saudio_setup(&(saudio_desc){
         .stream_cb = snd_clbk,
+        .num_channels = 2,
     });
 }
 
