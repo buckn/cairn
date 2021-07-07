ab9e016582c98552b6d7f25071447d6c55f403a1
buckn <git@buckn.dev>
2021-07-07 14:24:19 -0400

    snd update

--- snd.h
+++ snd.h
@@ -67,7 +67,7 @@ void snd_adsr(int t, float samples[2], int sample_rate, float adsr_t[4], float a
 //sound callback provided to sokol_audio.h
 void snd_clbk(float* buffer, int num_frames, int num_channels) {
     for (int i = 0; i < num_frames; i++) {
-    	if (cli.keys_down[SAPP_KEYCODE_SPACE]) {
+        if (cli.keys_down[SAPP_KEYCODE_SPACE]) {
             /*float frame_a[2] = {snd_sin(cli.snd_sin_wav, 440.0f, 1.0f, 44100), snd_sin(cli.snd_sin_wav + 55, 110.0f, 1.0f, 44100)};
             float frame_b[2] = {snd_sin(cli.snd_sin_wav, 880.0f, 1.0f, 44100), snd_sin(cli.snd_sin_wav + 55, 220.0f, 1.0f, 44100)};
             float frame_c[2] = {snd_add(frame_a, 0.5f), snd_add(frame_b, 0.5f)};*/
@@ -75,18 +75,18 @@ void snd_clbk(float* buffer, int num_frames, int num_channels) {
             float adsr_times[4] = {3.0f, 1.5f, 4.0f, 5.0f};
             float adsr_amplitudes[4] = {0.9f, 0.7f, 0.67f, 0.1f};
             snd_adsr(cli.snd_sin_wav, frame_d, 44100, adsr_times, adsr_amplitudes);
-    		buffer[num_channels * i] = snd_add(frame_d, 0.5f);  // mono channel
+            buffer[num_channels * i] = snd_add(frame_d, 0.5f);  // mono channel
             cli.snd_sin_wav++;
-    	} else {
-    		buffer[num_channels * i] = snd_sin(cli.snd_sin_wav, 1.0f, 0.0f, 44100);  // left channel
+        } else {
+            buffer[num_channels * i] = snd_sin(cli.snd_sin_wav, 1.0f, 0.0f, 44100);  // left channel
             cli.snd_sin_wav = 0;
-    	}
+        }
     }
 }
 
 //initalization fn for sokol_audio.h
 void snd_init() {
-	saudio_setup(&(saudio_desc) {
+    saudio_setup(&(saudio_desc) {
         .stream_cb = snd_clbk,
         .num_channels = 1,
         //.sample_rate = 44100,
@@ -96,5 +96,5 @@ void snd_init() {
 
 //shutdown fn for sokol_audio.h
 void snd_shutdown() {
-	saudio_shutdown();
+    saudio_shutdown();
 }
